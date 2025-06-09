beedd626c66bf0933559966eed00ebd0cbffab7e
MyPythonRepository <iryna.sharova@ukr.net>
2025-06-10 00:01:50 +0300

    HT_23

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render  # NOQA: F401
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpRequest
+from django.urls import reverse_lazy
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.views.generic import TemplateView, RedirectView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
 
-# Create your views here.
+from config import settings
+from .forms import UserRegistrationForm
+from .services.emails import send_registration_email
+from .utils.token_generator import TokenGenerator
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["some_key"] = {"name": "name"}
+        return context
+
+
+class UserLoginView(LoginView):
+    ...
+
+
+class UserLogoutView(LogoutView):
+    ...
+
+
+class UserRegistration(CreateView):
+    template_name = "registration/registration.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        self.object: get_user_model() = form.save(commit=False)
+        self.object.is_active = False
+        self.object.save()
+
+        send_registration_email(user_instance=self.object, request=self.request)
+
+        return super().form_valid(form)
+
+
+class UserActivationView(RedirectView):
+    url = reverse_lazy("index")
+
+    def get(self, request, uid, token, *args, **kwargs):
+        try:
+            pk = force_str(urlsafe_base64_decode(uid))
+            current_user = get_user_model().objects.get(pk=pk)
+        except (get_user_model().DoesNotExist, ValueError, TypeError):
+            return HttpResponse("Wrong data!")
+
+        if current_user and TokenGenerator().check_token(current_user, token):
+            current_user.is_active = True
+            current_user.save()
+
+            login(request, current_user)
+
+            return super().get(request, *args, **kwargs)
+
+        return HttpResponse("Wrong data!")
+
+
+@login_required(login_url=reverse_lazy("login"))
+def send_test_email(request: HttpRequest) -> HttpResponse:
+    send_mail(
+        subject="Darunky2025",
+        message="Darunky2025",
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[settings.EMAIL_HOST_USER],
+    )
+    return HttpResponse("Done")
--- src/config/urls.py
+++ src/config/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", IndexView.as_view(), name="index"),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ]
 
 if settings.DEBUG:
